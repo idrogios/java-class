5b76173923d4c29e59ef18541763f37eaa5996db
Idrogios Education <34035295+idrogios@users.noreply.github.com>
2017-12-01 15:01:10 +0200

    Add files via upload

---
+++ WP/src/MainClass.java
@@ -0,0 +1,12 @@
+
+public class MainClass {
+
+	public static void main(String[] args){
+		System.out.println("Hello World, again!");
+		NewFeatureAdded nfa = new NewFeatureAdded();
+		nfa.foo();
+	}
+	
+	
+}
+
---
+++ WP/src/NewFeatureAdded.java
@@ -0,0 +1,6 @@
+
+public class NewFeatureAdded {
+	public void foo(){
+		System.out.println("Hello from foo method");
+	}
+}
